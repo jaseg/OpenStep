8c689d897f968d6737cc44552f4d8115debeab08
jaseg <code@jaseg.net>
2015-02-09 02:14:54 +0100

    Working test code

--- firmware/main.c
+++ firmware/main.c
@@ -5,6 +5,17 @@
 #define PIEZO_1_CH  0
 #define PIEZO_2_CH  4
 #define PIEZO_3_CH  6
+#define ADC10CTL1_FLAGS_CH1 ((PIEZO_1_CH<<12) | ADC10DIV_3)
+#define ADC10CTL1_FLAGS_CH2 ((PIEZO_2_CH<<12) | ADC10DIV_3)
+#define ADC10CTL1_FLAGS_CH3 ((PIEZO_3_CH<<12) | ADC10DIV_3)
+
+#define RGB_R_PIN   0
+#define RGB_G_PIN   1
+#define RGB_B_PIN   4
+
+#define ST_YLW_PIN  2
+#define ST_GRN_PIN  3
+
 #define PIEZO_HIGHEST_CHANNEL   PIEZO_3_CH
 
 int uart_getc(){
@@ -21,57 +32,105 @@ char nibble_to_hex(uint8_t nibble){
     return (nibble < 0xA) ? ('0'+nibble) : ('A'+nibble-0xA);
 }
 
+void uart_puthword(uint16_t val){
+    uart_putc(nibble_to_hex(val>>12&0xF));
+    uart_putc(nibble_to_hex(val>>8&0xF));
+    uart_putc(nibble_to_hex(val>>4&0xF));
+    uart_putc(nibble_to_hex(val&0xF));
+}
+
 void uart_putadc(uint16_t val){
     uart_putc(nibble_to_hex(val>>8&0xF));
     uart_putc(nibble_to_hex(val>>4&0xF));
     uart_putc(nibble_to_hex(val&0xF));
 }
 
+volatile int cur_adc_ch;
+volatile int adc_res[3];
+
+void adc10_isr(void) __attribute__((interrupt(ADC10_VECTOR)));
+void adc10_isr(void) {
+    ADC10CTL0 &= ~ENC;
+    switch(cur_adc_ch){
+        case ADC10CTL1_FLAGS_CH1:
+            cur_adc_ch = ADC10CTL1_FLAGS_CH2;
+            adc_res[0] = ADC10MEM;
+            _BIS_SR_IRQ(LPM0_bits);
+            break;
+        case ADC10CTL1_FLAGS_CH2:
+            cur_adc_ch = ADC10CTL1_FLAGS_CH3;
+            adc_res[1] = ADC10MEM;
+            _BIS_SR_IRQ(LPM0_bits);
+            break;
+        case ADC10CTL1_FLAGS_CH3:
+            cur_adc_ch = ADC10CTL1_FLAGS_CH1;
+            adc_res[2] = ADC10MEM;
+            _BIC_SR_IRQ(LPM0_bits);
+            break;
+    }
+    ADC10CTL1  = cur_adc_ch;
+    ADC10CTL0 |= ENC | ADC10SC;
+}
+
 int main(void){
     WDTCTL = WDTPW | WDTHOLD; /* disable WDT */
 
-    /* ADC setup */
-    uint16_t results[PIEZO_HIGHEST_CHANNEL+1];
-
+    /* clock setup */
     DCOCTL      = CALDCO_16MHZ;
     BCSCTL1     = CALBC1_16MHZ;
-    ADC10CTL0  |= SREF0 | ADC10SHT1 | ADC10SHT0 | MSC | ADC10ON;
-    ADC10CTL1  |= CONSEQ0 | CONSEQ1 | (PIEZO_HIGHEST_CHANNEL<<12);
-    ADC10AE0   |= (1<<PIEZO_1_CH) | (1<<PIEZO_2_CH) | (1<<PIEZO_3_CH);
-    ADC10CTL0  |= ADC10SC | ENC;
 
-    ADC10DTC0  |= ADC10CT;
-    ADC10DTC1   = 3;
-    ADC10SA     = results;
+    /* ADC setup */
+    ADC10CTL1   = ADC10CTL1_FLAGS_CH2;
+    cur_adc_ch  = ADC10CTL1_FLAGS_CH2;
+    ADC10CTL0  |= SREF_0 | ADC10SHT_3 | ADC10ON | ADC10IE;
+    ADC10AE0   |= (1<<PIEZO_1_CH) | (1<<PIEZO_2_CH) | (1<<PIEZO_3_CH);
 
     /* UART setup */
     P1SEL      |= 0x06;
     P1SEL2     |= 0x06;
 
-    /* set for 115.2kBd @ 16MHz */
+    /* Set for 115.2kBd @ 16MHz */
     UCA0CTL1   |= UCSSEL1;
     UCA0BR0     = 138;
     UCA0BR1     = 0;
     UCA0MCTL    = UCBRS2;
     UCA0CTL1   &= ~UCSWRST;
 
+    /* PWM setup */
+/*    P2DIR      |= (1<<RGB_R_PIN) | (1<<RGB_G_PIN) | (1<<RGB_B_PIN);
+    P2SEL      |= (1<<RGB_R_PIN) | (1<<RGB_G_PIN) | (1<<RGB_B_PIN);
+
+    TA0CTL      = TASSEL_2 | ID_0 | MC_2;
+    TA0CCTL0    = OUTMOD_3;
+    TA0CCTL1    = OUTMOD_3;
+    TA0CCTL2    = OUTMOD_3;
+*/
+
+    /* Status LED setup */
+    P2DIR      |= (1<<ST_GRN_PIN) | (1<<ST_YLW_PIN);
+
     /* Set global interrupt enable flag */
     _BIS_SR(GIE);
 
+    /* Kick ADC... */
+    ADC10CTL0 |=  ENC | ADC10SC;
+
+    /* ...and go to sleep. */
+    _BIS_SR(LPM0_bits);
+
     while(42){
-        uint16_t ch1 = results[PIEZO_HIGHEST_CHANNEL-PIEZO_1_CH];
-        uint16_t ch2 = results[PIEZO_HIGHEST_CHANNEL-PIEZO_2_CH];
-        uint16_t ch3 = results[PIEZO_HIGHEST_CHANNEL-PIEZO_3_CH];
+//        TA0CCR0 = TA0CCR1 = TA0CCR2 = (ch1<<6);
+        P2OUT   ^= (1<<ST_YLW_PIN);
         uart_putc('R');
         uart_putc(' ');
-        uart_putadc(ch1);
+        uart_putadc(adc_res[0]);
         uart_putc(' ');
-        uart_putadc(ch2);
+        uart_putadc(adc_res[1]);
         uart_putc(' ');
-        uart_putadc(ch3);
+        uart_putadc(adc_res[2]);
         uart_putc('\r');
         uart_putc('\n');
-		__delay_cycles(1600000);
+        _BIS_SR(LPM0_bits);
     }
     return 0;
 }
