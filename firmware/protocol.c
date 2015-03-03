5db598907255252cf42f305a2fc02cd444a3d098
jaseg <code@jaseg.net>
2015-03-03 01:36:37 +0100

    Converted the discovery logic, half done converting the rest

--- firmware/protocol.c
+++ firmware/protocol.c
@@ -19,12 +19,6 @@
 
 uint16_t current_address = INVALID_ADDRESS;
 
-typedef struct {
-    uint8_t escaped:1;
-    uint8_t receiving_payload:1; /* used by some handlers */
-    int8_t ignore_count;
-} rx_state_t;
-
 typedef union {
     struct {
         uint16_t r, g, b;
@@ -32,164 +26,199 @@ typedef union {
     uint16_t ch[3];
 } led_pwm_t;
 
+#define PKT_BUF_LEN sizeof(_pkt_hack)
+#define PKT_MEM (_TYPE) struct __attribute__((__packed__)) { char _pad[PKT_BUF_LEN-sizeof(_TYPE)]; _TYPE; }
+ 
+union {
+    adc_res_t;
+    led_pwm_t;
+} _pkt_hack;
+
 typedef struct {
-    uint8_t node_id;
-    uint8_t cmd;
     union {
-        struct {
-            uint8_t new_id;
-            uint8_t mac_mask[8];
-        } discovery;
-        adc_res_t adc;
-        led_pwm_t pwm;
-    } payload;
+        PKT_MEM(dsc_inf_t);
+        PKT_MEM(adc_res_t);
+        PKT_MEM(led_pwm_t);
+    };
+    char _end[0];
 } pkt_t;
+
+#undef PKT_BUF_LEN
+#undef PKT_MEM
 
 /* protocol handling declarations */
 void ucarx_handler(void) __attribute__((interrupt(USCIAB0RX_VECTOR)));
-static unsigned int handle_discovery_packet(char *p, pkt_t *pkt, rx_state_t *state);
-static unsigned int handle_command_packet(pkt_t *pkt, rx_state_t *state);
-static unsigned int handle_broadcast_packet(pkt_t *pkt, rx_state_t *state);
+static int handle_discovery_packet(char *p, pkt_t *pkt, rx_state_t *state);
+static int handle_command(int broadcast, pkt_t *pkt, rx_state_t *state);
 
 void ucarx_handler() {
-	static rx_state_t state;
-	static pkt_t pkt;
+	static void *state;
 	static char* p;
-	static char* end;
+	static struct {
+        int escaped:1;
+        int broadcast:1;
+        unsigned int cmd:6;
+    } bits;
+	static pkt_t pkt;
 
     char c = UCA0RXBUF;
-
-	if (state.escaped) {
-        state.escaped = 0;
-		if (c == '?') {
-			state.ignore_count = 0;
-            state.receiving_payload = 0;
-			p = (char*)&pkt;
-			end = (char*)&pkt.payload;
-			return;
-		} else if (c == '!' && state.ignore_count > 0) {
-            state.ignore_count--;
+    
+    if (bits.escaped) {
+        if (c == '?') {
+            state = &&preamble;
+            p = pkt.end;
             return;
         }
-	} else if (c == '\\') {
-        state.escaped = 1;
+    } else if (c == '\\') {
+        bits.escaped = 1;
         return;
-	}
-	// escape sequence handling completed. c now contains the next char of the payload.
-
-	if (state.ignore_count)
-		return;
-    *p++ = c;
-
-	if (p == end) {
-        int n = 0;
-        if (pkt.node_id == DISCOVERY_ADDRESS) {
-            n = handle_discovery_packet(p, &pkt);
-        } else if (pkt.node_id == current_address) {
-            n = handle_command_packet(&pkt, &state);
-        } else if (pkt.node_id == BROADCAST_ADDRESS) {
-            n = handle_broadcast_packet(&pkt, &state);
-        }
-
-        //state.ignore_count |=-!n;
-        if (n < 0)
-            state.ignore_count = -n;
-        else if (n == 0)
-            state.ignore_count = -1;
-        else
-            end += n;
     }
-}
 
-inline static int handle_discovery_packet(char *p, pkt_t *pkt) {
-    if (p == (char*)&pkt->payload.discovery) {
-        return sizeof(pkt->payload.discovery)-1;
+    goto *state;
+
+preamble:
+    if (c == BROADCAST_ADDRESS) {
+        bits.broadcast = 1;
+        state = &&packet_pre;
+    } else if (c == current_address) {
+        bits.broadcast = 0;
+        state = &&packet_pre;
+    } else if (c == DISCOVERY_ADDRESS) {
+        p = &pkt.dsc_inf_t;
+        state = &&discovery;
     } else {
-        unsigned int bcnt = pkt->cmd>>1;
-        unsigned int nibble = pkt->cmd&1;
-        for (unsigned int i=0; i<bcnt; i++)
-            if (CONFIG_MAC[i] != pkt->payload.discovery.mac_mask[i])
-                return 0;
-        if(nibble)
-            if ((CONFIG_MAC[bcnt]&0xF0) != (pkt->payload.discovery.mac_mask[bcnt]&0xF0))
-                return 0;
-
-        rs485_enable();
-        uart_putc(0xFF); /* "I'm here!" */
-        rs485_disable();
-        current_address = pkt->payload.discovery.new_id;
+        state = &&idle;
     }
-    return 0;
+    return;
+
+discovery:
+    /* We are abusing bits.broadcast here to store whether the mask ends in the middle of a byte */
+    bits.broadcast = c&1;
+    /* We are abusing p here as a simple integer to store the number of remaining mask bytes */
+    p = (void*) ((c+1)>>1);
+    state = (c&1) ? &&discovery_nibble : &&discovery_match;
+    return;
+
+discovery_match:
+    if (CONFIG_MAC[(size_t) p] != c)
+        state = &&idle;
+    else if (!--p)
+        state = bits.broadcast ? &&discovery_nibble : &&discovery_response;
+    return;
+
+discovery_nibble:
+    if ((CONFIG_MAC[(size_t) p]&0xF0) != (c&0xF0))
+        state = &&idle;
+    else
+        state = &&discovery_response;
+    return;
+
+discovery_response:
+    current_address = c;
+    state = &&idle;
+    rs485_enable();
+    uart_putc(0xFF); /* "I'm here!" */
+    rs485_disable();
+    return;
+
+packet_pre:
+    int n = prepare_packet(bits.broadcast, c);
+    bits.cmd = c;
+    if (n > 0) {
+        state = &&packet_post;
+        p = pkt.end - n;
+    } else if (n < 0) {
+        state = &&packet_wait;
+        p = pkt.end + n;
+    } else {
+        handle_command(bits.broadcast, bits.cmd, &pkt);
+        state = &&idle;
+    }
+    return;
+
+packet_wait:
+    if (bits.escaped && c == '!') {
+        *p++ = c;
+        if (p == pkt.end) {
+            p -= wait_elapsed(bits.broadcast, bits.cmd);
+            state = &&packet_post;
+        }
+    }
+    return;
+
+packet_post:
+    *p++ = c;
+    if (p == pkt.end) {
+        handle_command(bits.broadcast, bits.cmd, &pkt);
+        state = &&idle;
+    }
+
+idle:
+    return;
 }
 
-inline static int handle_command_packet(pkt_t *pkt, rx_state_t *state) {
-    switch (pkt->cmd) {
+inline static int prepare_packet(int broadcast, char cmd) {
+    if (broadcast) {
+        switch (cmd) {
+        case CMD_SET_PWM:
+            return sizeof(led_pwm_t);
+
         case CMD_GET_DATA:
-            rs485_enable();
-            escaped_send(&adc_res);
-            rs485_disable();
-            break;
-        case CMD_SET_LEDS:
-            if (!state->receiving_payload) {
-                state->receiving_payload = 1;
-                return sizeof(pkt->payload.pwm);
-            } else {
-                state->receiving_payload = 0;
-                TA1CCR0 = pkt->payload.pwm.r;
-                TA1CCR1 = pkt->payload.pwm.g;
-                TA1CCR2 = pkt->payload.pwm.b;
-            }
-            break;
-        case CMD_STATUS_OFF:
-        case CMD_STATUS_GREEN:
-        case CMD_STATUS_YELLOW:
-        case CMD_STATUS_BOTH:
-            P2OUT      &= ~((1<<ST_GRN_PIN) | (1<<ST_YLW_PIN));
-            P2OUT      |= ((pkt->cmd&1) ? (1<<ST_GRN_PIN) : 0) | ((pkt->cmd&2) ? (1<<ST_YLW_PIN) : 0);
-            break;
-        case CMD_FLASH_LED:
-            P2DIR      |= (1<<ST_YLW_PIN);
-            P2OUT      |= (1<<ST_YLW_PIN);
-            __delay_cycles(8000000);
-            P2OUT      &= ~(1<<ST_YLW_PIN);
-            P2DIR      &= ~(1<<ST_YLW_PIN);
-            break;
+            /* A bit of arcane information on this: nodes are numbered continuously beginning from one by the master.
+             * payload position in the cluster-response is determined by the node's address. */
+            return -(current_address-1);
+        }
     }
     return 0;
 }
 
-inline static int handle_broadcast_packet(pkt_t *pkt, rx_state_t *state) {
-    switch (pkt->cmd) {
-        case BCMD_GET_DATA:
-            __delay_cycles(16000);
-            if (!state->just_counting) {
-                if (current_address == 0 ) {
-                    rs485_enable();
-                    __delay_cycles(16000);
-                    send_sof();
-                    escaped_send(&adc_res);
-                    __delay_cycles(16000);
-                    rs485_disable();
-                } else {
-                    state->just_counting = 1;
-                    /* bit of arcane information on this: nodes are numbered continuously beginning from one by the
-                     * master. payload position in the cluster-response is determined by the node's address. */
-                    return current_address-1;
-                }
-            } else {
-                state->just_counting = 0;
-                rs485_enable();
-                send_sof();
-                escaped_send(&adc_res);
-                rs485_disable();
-            }
-            break;
-        case BCMD_ACQUIRE:
-            kick_adc();
-            /* ...and go to sleep. */
-            _BIS_SR_IRQ(LPM0_bits); /* HACK this only works because this function is always inlined into the ISR */
-            break;
+inline static int wait_elapsed(int broadcast, char cmd) {
+    switch (cmd) {
+        case CMD_GET_DATA:
+            return sizeof(adc_res_t);
     }
     return 0;
+}
+
+inline static int handle_command(int broadcast, int cmd, pkt_t* pkt) {
+    switch (cmd) {
+    case CMD_SET_PWM:
+        TA1CCR0 = pkt->led_pwm_t.r;
+        /*TA1CCR1 = pkt->led_pwm_t.g;*/
+        TA1CCR2 = pkt->led_pwm_t.b;
+        break;
+
+    case CMD_STATUS_OFF:
+    case CMD_STATUS_GREEN:
+    case CMD_STATUS_YELLOW:
+    case CMD_STATUS_BOTH:
+        P2OUT      &= ~((1<<ST_GRN_PIN) | (1<<ST_YLW_PIN));
+        P2OUT      |= ((cmd&1) ? (1<<ST_GRN_PIN) : 0) | ((cmd&2) ? (1<<ST_YLW_PIN) : 0);
+        break;
+
+    case CMD_FLASH_LED:
+        P2DIR      |= (1<<ST_YLW_PIN);
+        P2OUT      |= (1<<ST_YLW_PIN);
+        __delay_cycles(8000000);
+        P2OUT      &= ~(1<<ST_YLW_PIN);
+        P2DIR      &= ~(1<<ST_YLW_PIN);
+        break;
+
+    case CMD_GET_DATA:
+        __delay_cycles(16000);
+        rs485_enable();
+        __delay_cycles(16000);
+        send_sof();
+        escaped_send(&adc_res);
+        __delay_cycles(16000);
+        rs485_disable();
+        break;
+
+    case CMD_ACQUIRE:
+        kick_adc();
+        /* ...and go to sleep. */
+        _BIS_SR_IRQ(LPM0_bits); /* HACK this only works because this function is always inlined into the ISR */
+        break;
+    }
 }
 
